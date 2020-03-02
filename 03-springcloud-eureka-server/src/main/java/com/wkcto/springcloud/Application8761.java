ec67c01f80ed2e85aebad55635f194feb83cf3b7
clown14 <785613198@qq.com>
2020-03-02 23:36:52 +0800

    eureka 高可用集群&自我保护机制

--- 03-springcloud-eureka-server/src/main/java/com/wkcto/springcloud/Application8761.java
+++ 03-springcloud-eureka-server/src/main/java/com/wkcto/springcloud/Application8761.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer //开启Eureka注册中心服务端
-public class Application {
+public class Application8761 {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application8761.class, args);
     }
 
 }
